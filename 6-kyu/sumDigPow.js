f1de8a724e2c29db7a5b7a5558e00e2d5bbc7433
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-05-17 13:17:14 -0400

    Create sumDigPow.js

---
+++ 6-kyu/sumDigPow.js
@@ -0,0 +1,34 @@
+// https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/javascript
+const sumDigPow = (a,b) => 
+{
+    const returnArr = [];
+    for (let i = a; i <= b; i++)
+    {
+        const numArr = (i + "").split("").map(x => parseInt(x)).map((value, pos) => Math.pow(value,pos + 1))
+        console.log(numArr)
+        const sum = numArr.reduce((total, curr) => total + curr, 0)
+        if (i === sum ) returnArr.push(i);
+        
+    }
+    return returnArr;
+}
+console.log(sumDigPow(89,89))
+/*
+The number 89 is the first integer with more than one digit that fulfills the property partially 
+introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
+
+In effect: 89 = 8^1 + 9^2
+
+The next number in having this property is 135.
+
+See this property again: 135 = 1^1 + 3^2 + 5^3
+
+We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] 
+(inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+Let's see some cases:
+
+sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+*/
