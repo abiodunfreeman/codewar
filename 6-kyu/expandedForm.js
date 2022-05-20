9f8dcee6420aad1d9b27e6dfcc6a15300b83df78
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-05-20 08:54:48 -0400

    expandedForm.js

--- 6-kyu/expandedForm.js
+++ 6-kyu/expandedForm.js
@@ -19,4 +19,12 @@ const expandArr = numSplit.map((value, pos) => {
 
 return expandArr.join(" + ");
 }
-console.log(expandedForm(70304)) // 3021 => "3000 + 20 + 1"
+console.log(expandedForm(3021)) // 3021 => "3000 + 20 + 1"
+
+/* 
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+*/
