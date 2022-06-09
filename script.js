7eb1541b63ff057785c5e79d01829221c9de5248
Odun Freeman <bornduringwar@gmail.com>
2022-06-09 12:42:38 -0400

    tried out typescript on this one

---
+++ script.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+const solution = (str, ending) => {
+    let find = ending.split('');
+    const strArr = str.split('');
+    const diff = strArr.length - find.length;
+    strArr.splice(0, diff);
+    console.log(strArr);
+    for (let i = 0; i < find.length; i++) {
+        if (find[i] !== strArr[i])
+            return false;
+    }
+    return true;
+};
+console.log(solution('abc', 'bc')); // => true
+/*
+  Complete the solution so that it returns true
+   if the first argument(string) passed in ends with the 2nd argument (also a string).
+  
+  Examples:
+  
+  solution('abc', 'bc') // returns true
+  solution('abc', 'd') // returns false
+  */
--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-s;
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+const solution = (str, ending) => {
+  const strArr = str.split('');
+};
+console.log(solution('abc', 'bc')); // => true
+/* 
+Complete the solution so that it returns true
+ if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
