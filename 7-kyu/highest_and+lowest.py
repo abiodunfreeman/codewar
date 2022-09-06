57804e93dd6f1a5347563d20fbb9bede75bcfd37
Odun Freeman <bornduringwar@gmail.com>
2022-09-06 02:20:53 -0400

    Create highest_and+lowest.py

---
+++ 7-kyu/highest_and+lowest.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
+def high_and_low(numbers):
+    numbers = numbers.split()
+    largest_number = int(numbers[0])
+    lowest_number = int(numbers[0])
+    pos = 0
+
+    for num in numbers: 
+        numbers[pos] = int(num)
+        pos = pos + 1
+
+    for num in numbers: 
+        if num > largest_number: 
+            largest_number = num
+        if num < lowest_number: 
+            lowest_number = num
+ 
+    numbers = str(largest_number) + " " + str(lowest_number)
+    return numbers
+
+print(high_and_low("1 3 48 30 -4 200 4893 20349 -20"))
+
